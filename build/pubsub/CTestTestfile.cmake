4ba2f95c6481f2aec2a8318000ccc8d349615674
asana <32097878+kaightasa@users.noreply.github.com>
2021-07-12 21:48:37 +0900

    Delete build directory

--- build/pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asana/ros2-foxy-analysis/pubsub
-# Build directory: /home/asana/ros2-foxy-analysis/build/pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
